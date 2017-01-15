531e3219096e3cb1aeee2eeb050e1ae8072413e0
mayuranchalia <mayur.anchalia@gmail.com>
2017-01-15 18:19:06 +0530

    Added gradle script for Artifactory publish

--- src/main/java/com/giridhari/preachingassistant/rest/model/GenericStatusEntity.java
+++ src/main/java/com/giridhari/preachingassistant/rest/model/GenericStatusEntity.java
@@ -1,8 +1,21 @@
 package com.giridhari.preachingassistant.rest.model;
 
 public class GenericStatusEntity extends ResponseEntity {
-	
+
 	private String status;
 	private String reason;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
 
 }
