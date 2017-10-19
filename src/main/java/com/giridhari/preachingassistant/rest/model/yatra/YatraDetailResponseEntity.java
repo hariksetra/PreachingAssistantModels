2d54f3c51c4b123fad8ecb54332372a281b78208
Hariksetra Das <hariksetra.jps@gmail.com>
2017-10-19 14:46:54 +0530

    Implemented Yatra with basic listing functionality

---
+++ src/main/java/com/giridhari/preachingassistant/rest/model/yatra/YatraDetailResponseEntity.java
@@ -0,0 +1,42 @@
+package com.giridhari.preachingassistant.rest.model.yatra;
+
+import com.giridhari.preachingassistant.model.YatraType;
+import com.giridhari.preachingassistant.rest.model.ResponseEntity;
+
+public class YatraDetailResponseEntity extends ResponseEntity{
+	private Long id;
+	private String yatraName;
+	private String yatraAddress;
+	private YatraType yatraType;
+	private Long yatraAdmin;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getYatraName() {
+		return yatraName;
+	}
+	public void setYatraName(String yatraName) {
+		this.yatraName = yatraName;
+	}
+	public String getYatraAddress() {
+		return yatraAddress;
+	}
+	public void setYatraAddress(String yatraAddress) {
+		this.yatraAddress = yatraAddress;
+	}
+	public YatraType getYatraType() {
+		return yatraType;
+	}
+	public void setYatraType(YatraType yatraType) {
+		this.yatraType = yatraType;
+	}
+	public Long getYatraAdmin() {
+		return yatraAdmin;
+	}
+	public void setYatraAdmin(Long yatraAdmin) {
+		this.yatraAdmin = yatraAdmin;
+	}
+}
