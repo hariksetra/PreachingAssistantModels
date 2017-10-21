52e4bb99eb328d6ab1811b37d799ef5bc396d7c1
abhishekikc <abhishekikc@gmail.com>
2017-10-21 16:09:31 +0530

    Fields added to paging according to Spring

--- src/main/java/com/giridhari/preachingassistant/rest/model/Paging.java
+++ src/main/java/com/giridhari/preachingassistant/rest/model/Paging.java
@@ -8,32 +8,63 @@ package com.giridhari.preachingassistant.rest.model;
  */
 public class Paging {
 	
-	private int page;
-	private int pageSize;
 	private int totalPages;
-	
-	public int getPage() {
-		return page;
-	}
-	
-	public void setPage(int page) {
-		this.page = page;
-	}
-	
-	public int getPageSize() {
-		return pageSize;
-	}
-	
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
+	private long totalElements;
+	private boolean isLast;
+	private int pageSize;
+	private int pageNumber;
+	private String sortedOrder;
+	private boolean isFirst;
+	private int numberOfElements;
 	
 	public int getTotalPages() {
 		return totalPages;
 	}
-	
 	public void setTotalPages(int totalPages) {
 		this.totalPages = totalPages;
 	}
-
+	public long getTotalElements() {
+		return totalElements;
+	}
+	public void setTotalElements(long totalElements) {
+		this.totalElements = totalElements;
+	}
+	public boolean isLast() {
+		return isLast;
+	}
+	public void setLast(boolean isLast) {
+		this.isLast = isLast;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getPageNumber() {
+		return pageNumber;
+	}
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+	public String getSortedOrder() {
+		return sortedOrder;
+	}
+	public void setSortedOrder(String sortedOrder) {
+		this.sortedOrder = sortedOrder;
+	}
+	public boolean isFirst() {
+		return isFirst;
+	}
+	public void setFirst(boolean isFirst) {
+		this.isFirst = isFirst;
+	}
+	public int getNumberOfElements() {
+		return numberOfElements;
+	}
+	public void setNumberOfElements(int numberOfElements) {
+		this.numberOfElements = numberOfElements;
+	}
+	
+	
 }
